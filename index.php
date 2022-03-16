20f1d6ae59e7d416a65e0e08b40a6d6dfeb244df
Justin Truong <justin.truongt@yahoo.com>
2022-03-16 20:58:47 +0000

    added halo content and images

--- index.php
+++ index.php
@@ -1,12 +1,47 @@
 <html>
 <head>
-<title>Bond Web Service Demo</title>
+<title>Halo Video Game Series</title>
 <style>
 body {font-family:georgia;}
+
+    .halo{
+    border:1px solid #E77DC2;
+    border-radius: 5px;
+    padding: 5px;
+    margin-bottom:5px;
+    position:relative;   
+  }
+ 
+  .pic{
+    position:absolute;
+    right:10px;
+    top:10px;
+  }
+
 </style>
 <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
 <script type="text/javascript">
+
+function haloTemplate(halo){
+  return `<div class="halo">
+       
+     
+    <b>Title: </b> ${halo.Title}<br /> 
+    <b>Year: </b> ${halo.Year}<br /> 
+    <b>Writer: </b> ${halo.Writer}<br /> 
+    <b>Producer: </b> ${halo.Producer}<br /> 
+    <b>Ratings: </b> ${halo.Ratings}<br /> 
+    <div class="pic"><img src="thumbnails/${halo.Image}"/></div>  `;
+}
+
+
+
+
+
+
+
+  
 $(document).ready(function() {  
 
 	$('.category').click(function(e){
@@ -20,17 +55,31 @@ $(document).ready(function() {
 });
 request.done(function( data ) {
   console.log(data); 
-  $("#filetitle").html(data.title); 
+  
+  //place the title of the webservice on page
+  $("#halostitle").html(data.title);
 
+  //clear previous films
+  $("#halos").html("");
+
+
+  //load each fil via template into div 
+  $.each(data.halos,function(key,value){
+      let str = haloTemplate(value);
+      $("<div></div>").html(str).appendTo("#halos");
+
+    
+  });
+  
   //load data on page so we can see it. 
   //$("#output").text(JSON.stringify(data));
-
+/*
   let myData = JSON.stringify(data,null,4);
 
 
   myData = "<pre>" + myData + "</pre>";
   $("#output").html(myData);
-
+*/
 
   
 });
@@ -48,13 +97,21 @@ request.fail(function(xhr, status, error) {
 </script>
 </head>
 	<body>
-	<h1>Bond Web Service</h1>
-		<a href="year" class="category">Bond Films By Year</a><br />
-		<a href="box" class="category">Bond Films By International Box Office Totals</a>
-		<h3 id="filmtitle">Title Will Go Here</h3>
-		<div id="films">
-			<p>Films will go here</p>
+	<h1>Halo Video Game</h1>
+		<a href="year" class="category">Halo By Year</a><br />
+		<a href="ratings" class="category">Halo by Rating</a>
+		<h3 id="halos">Halo</h3>
+		<div id="halos">
+			<p>Halos will go here</p>
 		</div>
+
+
+
+
+
+
+
+    
 		<div id="output">Results go here</div>
 	</body>
 </html>
